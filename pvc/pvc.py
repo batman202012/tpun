092b6db3f2ed1886ce2c3767c367e343e2240c72
Joseph <jnjbros2@gmail.com>
2022-09-09 12:44:16 -0700

    Update pvc.py

--- pvc/pvc.py
+++ pvc/pvc.py
@@ -471,14 +471,14 @@ class pvc(commands.Cog):
     @commands.command(name="vcsync")
     async def vcsync(self, ctx: commands.Context):
         self.log.info("clearing commands...")
-        self.bot.tree.remove_command("vc")
-        await self.bot.tree.sync()
+        self.bot.tree.remove_command("vc", ctx.guild)
+        await self.bot.tree.sync(ctx.guild)
 
         self.log.info("waiting to avoid rate limit...")
         await asyncio.sleep(1)
-        self.bot.tree.add_command(self.vc)
+        self.bot.tree.add_command(self.vc, ctx.guild)
         commands = [c.name for c in self.bot.tree.get_commands()]
         self.log.info("registered commands: %s", ", ".join(commands))
         self.log.info("syncing commands...")
-        await self.bot.tree.sync()
+        await self.bot.tree.sync(ctx.guild)
         await ctx.send("VC Commands were synced")
