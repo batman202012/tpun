12bace0ee09babd4b044e9fc20d03322719e931a
Joseph <jnjbros2@gmail.com>
2022-09-13 15:16:53 -0700

    Update __init__.py

--- pvc/__init__.py
+++ pvc/__init__.py
@@ -12,13 +12,21 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 async def setup(bot: Red) -> None:
     await bot.add_cog(pvc(bot))
+
     @bot.tree.context_menu(name="PVC Mute")
     async def contextmute(interaction: discord.Interaction,  user: discord.User) -> None:
+        config = pvc.Config.get_conf(
+            pvc,
+            identifier=365398642334498816
+        )
         author = interaction.user
         if user is None:
             await interaction.response.send_message("{0} Please mention a user to mute.".format(author.name), ephemeral=True)
         else:
-            voiceChannel = await pvc.vcOwnerRead(pvc, interaction.guild, author.id)
+            i = await config.guild(interaction.guild).owners()
+            for vcOwner, vcId in i.items():
+                if vcOwner == str(interaction.user):
+                    voiceChannel = bot.get_channel(int(vcId))
             if voiceChannel is not None and user.voice is not None:
                 await voiceChannel.set_permissions(user, view_channel=True, read_messages=True, send_messages=False, read_message_history=True, use_voice_activation=True, stream=False, connect=True, speak=False, reason="{0} muted {1} in their vc: {2}".format(author.name, user.name, voiceChannel.name))
                 if user.voice.channel.id == voiceChannel.id:
