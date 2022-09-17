c2f55b129be39f82c2fab9768725b95fcb9e8c12
Joseph <jnjbros2@gmail.com>
2022-09-17 11:45:14 -0700

    Update serverhud.py

--- serverhud/serverhud.py
+++ serverhud/serverhud.py
@@ -377,7 +377,7 @@ class serverhud(commands.Cog):
         if event.value == "join":
             memberList = ctx.guild.members
             await self.config.guild(ctx.guild).truememcount.set(len([m for m in memberList if not m.bot]))
-            await self.config.guild(ctx.guild).newmemcount.set(len([m for m in memberList if m.joined_at > datetime.today() - timedelta(days=1)]))
+            await self.config.guild(ctx.guild).newmemcount.set(len([m for m in memberList if m.joined_at > (datetime.utcnow() - timedelta(days=1))]))
             await self.members(ctx.guild)
             await self.boosters(ctx.guild)
             await ctx.reply("Test of the member join/leave event.", ephemeral=True)
