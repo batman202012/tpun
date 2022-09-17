fa3b23b5b350b4290dcc07e83cdbae70e2efcd27
Joseph <jnjbros2@gmail.com>
2022-09-17 12:06:17 -0700

    Use event name for test response

--- serverhud/serverhud.py
+++ serverhud/serverhud.py
@@ -385,6 +385,6 @@ class serverhud(commands.Cog):
             await self.config.guild(ctx.guild).newmemcount.set(len([m for m in memberList if m.joined_at > utc.localize(datetime.utcnow() - timedelta(days=1))]))
             await self.members(ctx.guild)
             await self.boosters(ctx.guild)
-            await ctx.send("Test of the member join/leave event.", ephemeral=True)
+            await ctx.send(f"{event.value} event was tested successfully.", ephemeral=True)
         else:
             await ctx.send("That is not a valid event do [p]help serverhud test for a list of events", ephemeral=True)
